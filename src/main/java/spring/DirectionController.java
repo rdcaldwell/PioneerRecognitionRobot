b61ad6abf9342eb26c660e9a736ac2c44f28bab2
rdcaldwell <rdcald@yahoo.com>
2018-04-10 13:52:18 -0400

    Camera controller

--- src/main/java/spring/DirectionController.java
+++ src/main/java/spring/DirectionController.java
@@ -1,8 +1,10 @@
 package spring;
 
+import com.mobilerobots.Aria.ArRobot;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import robot.PioneerRobot;
 import utils.Server;
 
 @CrossOrigin(origins = Server.WEB_LOCAL)
@@ -11,23 +13,42 @@ public class DirectionController {
 
     private int degrees = 0;
     private int meters = 0;
+    private ArRobot robot = PioneerRobot.getRobot();
 
     @RequestMapping("/left")
     public Direction turnLeft() {
+        robot.lock();
+        degrees += 45;
+        robot.setHeading(degrees);
+        robot.unlock();
         return new Direction("left");
     }
 
     @RequestMapping("/right")
     public Direction turnRight() {
+        robot.lock();
+        degrees -= 45;
+        robot.setHeading(degrees);
+        robot.unlock();
         return new Direction("right");
     }
 
     @RequestMapping("/forward")
     public Direction moveForward() {
+        robot.lock();
+        meters += 1000;
+        robot.move(meters);
+        robot.unlock();
         return new Direction("forward");
     }
 
     @RequestMapping("/backward")
-    public Direction moveBackward() { return new Direction("backward"); }
+    public Direction moveBackward() {
+        robot.lock();
+        meters -= 1000;
+        robot.move(meters);
+        robot.unlock();
+        return new Direction("backward");
+    }
 
 }
